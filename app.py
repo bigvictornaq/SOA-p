31153c84c05047f0c93c588d44c3c993df327b45
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2020-09-25 21:29:44 -0600

    Se mostrar datos la tabla, ademas see mojoro el diseno

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 from os import getenv
 from dotenv  import load_dotenv
 
-from models import Result
+
 
 load_dotenv()
 
@@ -18,11 +18,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 print(app_settings)
 db = SQLAlchemy(app)
 
+from models import Result
 
-
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+   # Result = {}
+    if request.method == "POST":
+          try:
+              url = request.form['content']
+              new_country = Result(pais=url)
+              db.session.add(new_country)
+              db.session.commit()
+              return redirect('/')
+          except:
+              errors.append(
+                  "imposible de obtener url.Favorcito de hacer bien la buscaqueda carnal"
+              )
+    contenido = Result.query.order_by(Result.country_id).all()
+    return render_template('index.html',errors=errors,contenido=contenido)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,11 +2,11 @@ from app import db
 
 class Result(db.Model):
      __tablename__ = 'country'
-     id = db.Column(db.Integer,primary_key=True)
-     pais = db.Column(db.String())
-     fehca = db.Column(db.String())
-     def __init__(self,pais,fehca):
-          self.pais = pais
-          self.fehca = fehca
+     country_id = db.Column(db.Integer,primary_key=True)
+     country = db.Column(db.String())
+     last_update = db.Column(db.String())
+     def __init__(self,country,last_update):
+          self.country = country
+          self.last_update = last_update
      def __repr__(self):
-         return '<id{}>'.format(self.id)      
+         return '<country_id{}>'.format(self.country_id)      
